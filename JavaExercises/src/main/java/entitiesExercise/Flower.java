2a6fe531fff87c4e9ee9b8dd435cead3a4eb70c6
Maria Sanchez <msanchez157@gmail.com>
2021-04-19 17:37:10 -0400

    Flower class for Entity Exercise 1

---
+++ JavaExercises/src/main/java/entitiesExercise/Flower.java
@@ -0,0 +1,40 @@
+/** Udacity: Delivering Flowers Service Entity Exercise 1
+ *
+ *  The Flower should have a name, color, and price attribute. Flowers will be stored in a table called ‘plant’.
+ *  Make sure the price is stored as a DECIMAL column with 12 precision and 4 decimal places (scale). Make sure
+ *  that the flower name will support international language characters and that getters and setters are added to
+ *  the class.
+ */
+
+package entitiesExercise;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.Nationalized;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+
+@Entity
+@Getter @Setter @NoArgsConstructor
+@Table(name="plant")
+public class Flower {
+
+    @Id
+    @GeneratedValue
+    private Long Id;
+
+    @Nationalized // name will support international language characters
+    private String name;
+    private String color;
+    @Column(precision=12, scale=4) // 12 precision and 4 decimal places
+    private BigDecimal price;
+
+    public Flower(Long id, String name, String color, BigDecimal price) {
+        Id = id;
+        this.name = name;
+        this.color = color;
+        this.price = price;
+    }
+}
